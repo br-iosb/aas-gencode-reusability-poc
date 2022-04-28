a9963ff7284233d11a4001d5bc96e2d5d8be9a2d
roessba <bastian.roessl@iosb-ina.fraunhofer.de>
2022-04-28 13:32:13 +0200

    adding reference

--- CustomServerWebApplication/Startup.cs
+++ CustomServerWebApplication/Startup.cs
@@ -21,7 +21,7 @@ using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
-using IO.Swagger.Filters;
+using AAS.Server.Filters;
 
 
 namespace AAS.MyServerApp
