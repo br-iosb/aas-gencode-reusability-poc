cb577715b5c44a9ef12965cd4e3e9040fa7cec71
roessba <bastian.roessl@iosb-ina.fraunhofer.de>
2022-05-11 11:46:38 +0200

    use case for customization

--- poc/CustomClientApplication/Program.cs
+++ poc/CustomClientApplication/Program.cs
@@ -13,8 +13,12 @@ namespace CustomClientApplication
             Console.WriteLine("Hello API Client!");
             var clientWithConfig = new ApiClient(new Configuration());
 
+            //USECASE PROXY
             //customize underlying RestClient, e.g. Proxy
             clientWithConfig.RestClient.Proxy = new WebProxy();
+
+            //USECASE Client Certificate
+            clientWithConfig.RestClient.ClientCertificates = null;
 
             //can I modify underlying RestClient from the outside?
             var myCustomConfig = new Configuration();
@@ -24,9 +28,36 @@ namespace CustomClientApplication
             var fileServerApi = new AASXFileServerInterfaceApi(new Configuration());
             byte[] result = fileServerApi.GetAASXByPackageId("mypackageid");
 
-
-            //delegate file writers for e.g. progress bar
+            //USECASE UPLOAD
+            //(1) delegate file writers for e.g. progress bar for upload
             //new FileParameter { Writer = new Action<System.IO.Stream>() { Method } }
+
+            //USECASE DOWNLOAD
+            //(2) intercept IRestRequest with ResponseWriter and handle output to file incl. progress
+            //new RestRequest().ResponseWriter
+
+
         }
     }
+    /*
+     * Rules for Partial Classes
+All the partial class definitions must be in the same assembly and namespace.
+All the parts must have the same accessibility like public or private, etc.
+If any part is declared abstract, sealed or base type then the whole class is declared of the same type.
+Different parts can have different base types and so the final class will inherit all the base types.
+The Partial modifier can only appear immediately before the keywords class, struct, or interface.
+Nested partial types are allowed.
+    ==> not applicable outside the assembly
+    public class WithInterceptApiClient : ApiClient
+    {
+        public override void InterceptRequest(IRestRequest request) { }
+        //hint partial method
+
+        /// <summary>
+        /// Allows for extending response processing for <see cref="ApiClient"/> generated code.
+        /// </summary>
+        /// <param name="request">The RestSharp request object</param>
+        /// <param name="response">The RestSharp response object</param>
+        public override void InterceptResponse(IRestRequest request, IRestResponse response) { } //
+    }*/
 }
